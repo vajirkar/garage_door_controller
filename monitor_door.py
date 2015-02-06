89605f308b54a02660fc030ce99a01f70b46cf62
Siddharth Vajirkar <vajirkar@yahoo.com>
2015-02-05 23:42:52 -0800

    Added email notification, and repeat notifications.

--- monitor_door.py
+++ monitor_door.py
@@ -1,7 +1,9 @@
+import sys
 import RPi.GPIO as GPIO
 from datetime import datetime
 from datetime import timedelta
 import time
+import send_email as send_email
 
 CLOSED = 0
 OPEN = 1
@@ -10,20 +12,29 @@ door_state = CLOSED
 notstamped = timedelta(days=-100)
 door_open_since = notstamped
 warn_if_open_for_secs = 10
+repeat_notification_secs = 30
 notification_sent = False
-
-
-def send_email_notification():
-    
+last_notification = notstamped
 
 def send_notification():
     global notification_sent
+    global last_notification
     if notification_sent == False:
+        last_notification = datetime.now()
         ''' send notification '''
         notification_sent = True
-        send_email_notification()
+        msg = "Garage door open since " + door_open_since.strftime("%-I:%M%P %B %d, %Y") + "."
+        msg = msg + " Time now " + datetime.now().strftime("%-I:%M%P %B %d, %Y") + "."
+        send_email.send_email_notification(msg)
         '''send_sms_notification()'''
         '''send_tweet_notification()'''
+    elif datetime.now() - last_notification > timedelta(seconds=repeat_notification_secs):
+        last_notification = datetime.now()
+        print "Sending repeat notification ... %s." % last_notification
+        msg = "Garage door open since " + door_open_since.strftime("%-I:%M%P %B %d, %Y") + "."
+        msg = msg + " Time now " + datetime.now().strftime("%-I:%M%P %B %d, %Y") + "."
+        send_email.send_email_notification(msg)
+        
 
 def print_door_state():
     if door_state == OPEN:
@@ -39,14 +50,15 @@ def update_timestamp():
             door_open_since = datetime.now()
         else:
             if datetime.now() - door_open_since > timedelta(seconds=warn_if_open_for_secs):
-                print "Sending notification ... %s." % datetime.now()
                 send_notification()
 
     if door_state == CLOSED:
         door_open_since = notstamped
         notification_sent = False
+        last_notification = notstamped
 
 if __name__ == '__main__':
+    sys.path.append("/home/pi/gdc_settings")
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(door_switch_pin, GPIO.IN)
     door_state = GPIO.input(door_switch_pin)
