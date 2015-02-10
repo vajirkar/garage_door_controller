6b11e48534a3e821ffe41a6751c9db1b76a870e0
Siddharth Vajirkar <vajirkar@yahoo.com>
2015-02-09 17:01:18 -0800

    Added code for sending heartbeats via email.

--- monitor_door.py
+++ monitor_door.py
@@ -11,10 +11,16 @@ door_switch_pin = 3
 door_state = CLOSED 
 notstamped = timedelta(days=-100)
 door_open_since = notstamped
+poll_freq_secs = 1
 warn_if_open_for_secs = 10
 repeat_notification_secs = 30
 notification_sent = False
 last_notification = notstamped
+last_heartbeat = notstamped
+heartbeat_freq_hours = 24 
+heartbeat_freq_hours_ = heartbeat_freq_hours
+heartbeat_reattempt_freq_hours = 1
+heartbeat_send_failures = 0
 
 def send_notification():
     global notification_sent
@@ -26,6 +32,7 @@ def send_notification():
         msg = "Garage door open since " + door_open_since.strftime("%-I:%M%P %B %d, %Y") + "."
         msg = msg + " Time now " + datetime.now().strftime("%-I:%M%P %B %d, %Y") + "."
 	try:
+            print "Sending notification ... %s." % datetime.now()
             send_email.send_email_notification(msg)
         except:
             ''' Ignore for now, re-notification will be sent '''
@@ -43,6 +50,20 @@ def send_notification():
             ''' Ignore for now, re-notification will be sent '''
             print "send_email error, re-notification failed... will re-notify later."
         
+def send_heartbeat():
+    global last_heartbeat
+
+    msg = datetime.now().strftime("%-I:%M%P %B %d, %Y") +  " - Garage door monitor: All systems nominal. Heartbeat send failures = " + str(heartbeat_send_failures) + "."
+    try:
+        send_email.send_email_notification(msg)
+        last_heartbeat = datetime.now()
+        heartbeat_freq_hours = heartbeat_freq_hours_
+        heartbeat_send_failures = 0
+    except:
+        ''' Ignore for now, re-notification will be sent '''
+        print "send_email error, heartbeat failed... will heartbeat later."
+        heartbeat_freq_hours = heartbeat_reattempt_freq_hours
+        heartbeat_send_failures = heartbeat_send_failures + 1
 
 def print_door_state():
     if door_state == OPEN:
@@ -73,6 +94,7 @@ if __name__ == '__main__':
     print_door_state()
     ''' Setup the timestamp correctly upon startup, door could be open or closed '''
     update_timestamp()
+    last_heartbeat = datetime.now()
     while True:
         polled_door_state = GPIO.input(door_switch_pin)
         ''' If door changed state, then update timestamp '''
@@ -84,4 +106,6 @@ if __name__ == '__main__':
         elif door_state == OPEN:
             update_timestamp()
 
-        time.sleep(1)
+        if datetime.now() - last_heartbeat > timedelta(hours=heartbeat_freq_hours):
+            send_heartbeat()
+        time.sleep(poll_freq_secs)
