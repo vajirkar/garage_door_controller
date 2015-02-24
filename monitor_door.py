b01f82cb4e9c0a4f83346a8cb2a32ee1fcc458af
Siddharth Vajirkar <vajirkar@yahoo.com>
2015-02-24 01:22:32 -0800

    Added code to monitor two sensors. Better GPIO cleanup. Sends door closed notification also now.

--- monitor_door.py
+++ monitor_door.py
@@ -7,7 +7,8 @@ import send_email as send_email
 
 CLOSED = 0
 OPEN = 1
-door_switch_pin = 3
+door1_switch_pin = 3
+door2_switch_pin = 5
 door_state = CLOSED 
 notstamped = timedelta(days=-100)
 door_open_since = notstamped
@@ -15,6 +16,7 @@ poll_freq_secs = 1
 warn_if_open_for_secs = 10
 repeat_notification_secs = 30
 notification_sent = False
+send_closed_notification = True
 last_notification = notstamped
 last_heartbeat = notstamped
 heartbeat_freq_hours = 24 
@@ -25,6 +27,7 @@ heartbeat_send_failures = 0
 def send_notification():
     global notification_sent
     global last_notification
+    email_subject = "Garage Door Open Notification!"
     if notification_sent == False:
         last_notification = datetime.now()
         ''' send notification '''
@@ -33,7 +36,7 @@ def send_notification():
         msg = msg + " Time now " + datetime.now().strftime("%-I:%M%P %B %d, %Y") + "."
 	try:
             print "Sending notification ... %s." % datetime.now()
-            send_email.send_email_notification(msg)
+            send_email.send_email_notification(email_subject, msg)
         except:
             ''' Ignore for now, re-notification will be sent '''
             print "send_email error... will re-notify later."
@@ -45,17 +48,25 @@ def send_notification():
         msg = "Garage door open since " + door_open_since.strftime("%-I:%M%P %B %d, %Y") + "."
         msg = msg + " Time now " + datetime.now().strftime("%-I:%M%P %B %d, %Y") + "."
         try:
-            send_email.send_email_notification(msg)
+            send_email.send_email_notification(email_subject, msg)
         except:
             ''' Ignore for now, re-notification will be sent '''
             print "send_email error, re-notification failed... will re-notify later."
         
+def send_closed_notification():
+    msg = "Garage doors now closed at " + datetime.now().strftime("%-I:%M%P %B %d, %Y") + "."
+    try:
+        send_email.send_email_notification("Garage Door Closed Notification.", msg)
+    except:
+        ''' Ignore for now. Closed notification failures not to be re-sent. '''
+        print "send_email error, door closed notification not sent. Will not retry."
+
 def send_heartbeat():
     global last_heartbeat
 
     msg = datetime.now().strftime("%-I:%M%P %B %d, %Y") +  " - Garage door monitor: All systems nominal. Heartbeat send failures = " + str(heartbeat_send_failures) + "."
     try:
-        send_email.send_email_notification(msg)
+        send_email.send_email_notification("Garage Door Monitor Status.", msg)
         last_heartbeat = datetime.now()
         heartbeat_freq_hours = heartbeat_freq_hours_
         heartbeat_send_failures = 0
@@ -82,6 +93,8 @@ def update_timestamp():
                 send_notification()
 
     if door_state == CLOSED:
+        if notification_sent and send_closed_notification:
+            send_closed_notification()
         door_open_since = notstamped
         notification_sent = False
         last_notification = notstamped
@@ -89,23 +102,33 @@ def update_timestamp():
 if __name__ == '__main__':
     sys.path.append("/home/pi/gdc_settings")
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(door_switch_pin, GPIO.IN)
-    door_state = GPIO.input(door_switch_pin)
+    GPIO.setup(door1_switch_pin, GPIO.IN)
+    GPIO.setup(door2_switch_pin, GPIO.IN)
+    door_state = GPIO.input(door1_switch_pin) | GPIO.input(door2_switch_pin)
     print_door_state()
     ''' Setup the timestamp correctly upon startup, door could be open or closed '''
     update_timestamp()
     last_heartbeat = datetime.now()
-    while True:
-        polled_door_state = GPIO.input(door_switch_pin)
-        ''' If door changed state, then update timestamp '''
-        if door_state != polled_door_state:
-            door_state = polled_door_state
-            print_door_state()
-            update_timestamp()
-            ''' If door is open, then check for how long '''
-        elif door_state == OPEN:
-            update_timestamp()
+    try:
+        while True:
+            polled_door_state = GPIO.input(door1_switch_pin) | GPIO.input(door2_switch_pin)
+            ''' If door changed state, then update timestamp '''
+            if door_state != polled_door_state:
+                door_state = polled_door_state
+                print_door_state()
+                update_timestamp()
+                ''' If door is open, then check for how long '''
+            elif door_state == OPEN:
+                update_timestamp()
 
-        if datetime.now() - last_heartbeat > timedelta(hours=heartbeat_freq_hours):
-            send_heartbeat()
-        time.sleep(poll_freq_secs)
+            if datetime.now() - last_heartbeat > timedelta(hours=heartbeat_freq_hours):
+                send_heartbeat()
+            time.sleep(poll_freq_secs)
+    except KeyboardInterrupt:
+        print "Stopped by user."
+
+    except:
+        print "Exiting, some exception occurred."
+
+    finally:
+        GPIO.cleanup() # This ensures a clean exit.
--- send_email.py
+++ send_email.py
@@ -5,9 +5,9 @@ import gdc_settings as config
 # Import the email modules we'll need
 from email.mime.text import MIMEText
 
-def send_email_notification(textmsg):
+def send_email_notification(subject, textmsg):
 	msg = MIMEText(textmsg)
-	msg['Subject'] = "Garage Door Open Notification!"
+	msg['Subject'] = subject
 	msg['From'] = config.email_from
 	msg['To'] = config.email_to[0]
 	s = smtplib.SMTP('smtp.gmail.com', 587)
